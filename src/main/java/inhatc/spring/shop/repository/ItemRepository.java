5b593ef3f94c22896f6d6048a25a9f935a42ba1d
TaeJeongPark <ksmpjc99@gmail.com>
2023-09-20 13:46:25 +0900

    스프링프레임워크 4주차: QueryString, JPQL, QueryDsl

--- src/main/java/inhatc/spring/shop/repository/ItemRepository.java
+++ src/main/java/inhatc/spring/shop/repository/ItemRepository.java
@@ -2,6 +2,10 @@ package inhatc.spring.shop.repository;
 
 import inhatc.spring.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * packageName    : inhatc.spring.shop.repository
@@ -16,6 +20,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemNm(String itemNm);
 
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(int price);
+
+    @Query("SELECT i FROM Item i WHERE i.itemDetail LIKE %:itemDetail% ORDER BY i.price DESC")
+    List<Item> findByDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value = "SELECT * FROM item i WHERE i.item_detail LIKE %:itemDetail% ORDER BY i.price DESC", nativeQuery = true)
+    List<Item> findByDetailNative(@Param("itemDetail") String itemDetail);
 
 }
--- src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -1,16 +1,19 @@
 package inhatc.spring.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import inhatc.spring.shop.constant.ItemSellStatus;
 import inhatc.spring.shop.entity.Item;
-import jakarta.transaction.Transactional;
+import inhatc.spring.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 /**
  * packageName    : inhatc.spring.shop.repository
@@ -30,9 +33,13 @@ class ItemRepositoryTest {
     @Autowired  // 자동으로 구현체 생성
     private ItemRepository itemRepository;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Test
     @DisplayName("상품 생성 테스트")
     public void createItem() {
+
         Item item = Item.builder()
                 .itemNm("테스트 상품")
                 .price(10000)
@@ -47,6 +54,107 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
 
         System.out.println("============== savedItem : " + savedItem);
+
+    }
+
+    public void createItemList() {
+
+        for (int i = 1; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+
+            itemRepository.save(item);
+        }
+
+    }
+
+    @Test
+    @DisplayName("상품 이름 검색 테스트")
+    public void findByItemNmTest() {
+
+        createItemList();
+
+        // 람다 표기법
+        itemRepository
+                .findByItemNm("테스트 상품1")
+                .forEach((item) -> {
+            System.out.println(item);
+        });
+
+//        itemRepository.findByItemNm("테스트 상품1").forEach(System.out::println);
+//        itemList.forEach((item) -> System.out.println(item));
+//        itemList.forEach(System.out::println);
+
+    }
+
+    @Test
+    @DisplayName("OR 테스트")
+    public void findByItemNmOrItemDetailTest() {
+
+        createItemList();
+        itemRepository
+                .findByItemNmOrItemDetail("테스트 상품2", "테스트 상품 상세 설명8")
+                .forEach((item) -> {
+                    System.out.println(item);
+                });
+
+    }
+
+    @Test
+    @DisplayName("OrderBy 테스트")
+    public void findByPriceLessThanOrderByPriceDescTest() {
+
+        createItemList();
+        itemRepository
+                .findByPriceLessThanOrderByPriceDesc(10005)
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("JPQL 테스트")
+    public void findByDetailTest() {
+
+        createItemList();
+        itemRepository
+                .findByDetail("1")
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("Native 테스트")
+    public void findByDetailNativeTest() {
+
+        createItemList();
+        itemRepository
+                .findByDetailNative("1")
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("querydsl 테스트")
+    public void querydslTest() {
+
+        createItemList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QItem qItem = QItem.item;
+        List<Item> itemList = query.selectFrom(QItem.item)
+                .where(QItem.item.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(QItem.item.itemDetail.like("%" + "1" + "%"))
+                .orderBy(QItem.item.price.desc())
+                .fetch();
+
+        itemList.forEach(item -> System.out.println(item));
+
     }
 
 }
