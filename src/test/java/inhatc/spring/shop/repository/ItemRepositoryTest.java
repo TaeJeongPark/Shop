e72f3655734c22cc1209025911a4079be5c2a49d
TaeJeongPark <ksmpjc99@gmail.com>
2023-09-27 13:47:58 +0900

    Test Code Merge

--- src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package inhatc.spring.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import inhatc.spring.shop.constant.ItemSellStatus;
@@ -11,6 +12,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -59,7 +64,7 @@ class ItemRepositoryTest {
 
     public void createItemList() {
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 200; i++) {
             Item item = Item.builder()
                     .itemNm("테스트 상품" + i)
                     .price(10000 + i)
@@ -154,6 +159,112 @@ class ItemRepositoryTest {
                 .fetch();
 
         itemList.forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("query method Greater Than Equal And Like 테스트")
+    public void findByStockNumberGreaterThanEqualAndItemNmLikeTest() {
+
+        createItemList();
+        itemRepository
+                .findByStockNumberGreaterThanEqualAndItemNmLike(160, "%5%")
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("JPQL Greater Than Equal And Like 테스트")
+    public void findByStockAndNameTest() {
+
+        createItemList();
+        itemRepository
+                .findByStockAndName(160, "%5%")
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("Native Greater Than Equal And Like 테스트")
+    public void findByStockAndNameNativeTest() {
+
+        createItemList();
+        itemRepository
+                .findByStockAndNameNative(160, "%5%")
+                .forEach(item -> System.out.println(item));
+
+    }
+
+    @Test
+    @DisplayName("querydsl Greater Than Equal And Like 테스트")
+    public void findByStockAndNameQuerydslTest() {
+
+        createItemList();
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QItem qItem = QItem.item;
+        query.selectFrom(qItem)
+                .where(qItem.stockNumber.goe(160))
+                .where(qItem.itemNm.like("%" + "5" + "%"))
+                .fetch().forEach(item -> System.out.println(item));
+
+    }
+
+    public void createItemList2() {
+
+        for (int i = 1; i <= 5; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+
+            itemRepository.save(item);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SOLD_OUT)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+
+            itemRepository.save(item);
+        }
+
+    }
+
+    @Test
+    @DisplayName("querydsl 테스트2")
+    public void querydslTest2() {
+
+        createItemList2();
+
+        BooleanBuilder builder = new BooleanBuilder();  // 값이 존재하면 사용하고, 존재하지 않으면 사용하지 않는다.
+        String itemDetail = "테스트";
+        int price = 10002;
+        String itemSellStatus = "SELL";
+
+        QItem item = QItem.item;
+        builder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        builder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)) {
+            builder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+
+        Page<Item> page = itemRepository.findAll(builder, pageable);
+        List<Item> content = page.getContent();
+        content.stream().forEach(e -> System.out.println(e));
 
     }
 
