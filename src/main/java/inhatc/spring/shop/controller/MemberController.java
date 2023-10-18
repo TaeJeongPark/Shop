b7f723cacbcae5cfdd7a114ec2684bb8f3ded061
TaeJeongPark <ksmpjc99@gmail.com>
2023-10-18 11:29:12 +0900

    스프링프레임워크 8주차: 사용자 등록 폼

--- src/main/java/inhatc/spring/shop/controller/MemberController.java
+++ src/main/java/inhatc/spring/shop/controller/MemberController.java
@@ -1,11 +1,15 @@
 package inhatc.spring.shop.controller;
 
 import inhatc.spring.shop.dto.MemberFormDto;
+import inhatc.spring.shop.entity.Member;
 import inhatc.spring.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * packageName    : inhatc.spring.shop.controller
@@ -20,15 +24,29 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/member/new")
     public String memberForm(Model model) {
 
-//        model.addAttribute("memberFormDto", new MemberFormDto());
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+
+    }
+
+    @PostMapping("/member/new")
+    public String insertMember(MemberFormDto memberFormDto) {
+
+        log.info("==========> ", memberFormDto);
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
 
     }
 
